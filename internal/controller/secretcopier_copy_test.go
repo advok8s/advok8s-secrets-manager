0fa36c62956757c447497b3a335d6940ff84589d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2026-05-29 11:32:14 +1000

    Update copyright headers to 2024-2026 year range
    Change 'Copyright 2024 Graham Dumpleton' to 'Copyright 2024-2026 Graham
    Dumpleton' across all source files, the README, and hack/boilerplate.go.txt
    (the template controller-gen uses) so regenerated files stay consistent.

--- internal/controller/secretcopier_copy_test.go
+++ internal/controller/secretcopier_copy_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Graham Dumpleton.
+Copyright 2024-2026 Graham Dumpleton.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
